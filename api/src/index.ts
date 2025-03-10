39e21a671a9c6660c39d8d8efca381623e920d87
SyncSched <syncsched1@gmail.com>
2025-03-10 15:55:16 +0530

    EventBridge for cron job

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,17 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import serverless from 'aws-serverless-express';
 import app from './app';
+import { processNotifications } from './cron/RemainderCron';
 
 const server = serverless.createServer(app);
 
-export const handler = (event: APIGatewayProxyEvent, context: Context) => {
+export const handler = async (event: any, context: Context) => {
+  if (event.source === 'aws.events') {
+    console.log('🔔 Scheduled Event Triggered by EventBridge');
+    await processNotifications(); // Run cron job logic
+    return { statusCode: 200, body: 'Notifications processed' };
+  }
+
+  // Default API Gateway request handling
   return serverless.proxy(server, event, context);
 };
