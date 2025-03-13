fef595a34dc90e4b52e72c975aed33a346c3bd15
SyncSched <syncsched1@gmail.com>
2025-03-13 23:51:25 +0530

    solving the Main Remainder issue

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,15 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import serverless from 'aws-serverless-express';
 import app from './app';
+import { processNotifications } from './cron/RemainderCron';
 
 const server = serverless.createServer(app);
 
-export const handler = (event: APIGatewayProxyEvent, context: Context) => {
+export const handler = async (event: any, context: Context) => {
   console.log('🔍 Event Received:', JSON.stringify(event, null, 2));
+
+  if(event.source === "aws.scheduler"){
+    return await processNotifications();
+  }
   return serverless.proxy(server, event, context);
 };
